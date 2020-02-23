5d7bfed283d0d7b83dc1f56296e530024e6a8234
JohnSmith <dropp.boxx@mail.ru>
2020-02-23 13:06:39 +0300

    init

---
+++ src/VehicleManagementAPI/Controllers/VehicleController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;  
+
+namespace VehicleManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VehicleController : ControllerBase
+    {
+        private readonly ILogger<VehicleController> _logger;
+
+        public VehicleController(ILogger<VehicleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            try
+            {
+                var factory = new ConnectionFactory{
+                    HostName = "172.17.0.1",
+                    Port = 5672,
+                    UserName = "root",
+                    Password = "root",
+                }; 
+    
+                using(var connection = factory.CreateConnection())
+                using(var model = connection.CreateModel()){
+                    model.QueueDeclare("testQueue", 
+                                       durable: false,
+                                       exclusive: false,
+                                       autoDelete: false,
+                                       arguments: null);
+                                       
+                    var message = "Message, my message.";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    model.BasicPublish(exchange: string.Empty,
+                                       routingKey: "testQueue", 
+                                       basicProperties: null,
+                                       body: body);
+                }
+
+                return "Done";
+            }
+            catch (Exception ex) 
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
---
+++ src/WorkshopManagementAPI/Controllers/WorkshopController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;  
+using RabbitMQ.Client.Events;
+
+namespace WorkshopManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WorkshopController : ControllerBase
+    {
+        private readonly ILogger<WorkshopController> _logger;
+
+        public WorkshopController(ILogger<WorkshopController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            try
+            {
+                var factory = new ConnectionFactory{
+                    HostName = "172.17.0.1",
+                    Port = 5672,
+                    UserName = "root",
+                    Password = "root",
+                }; 
+
+                using(var connection = factory.CreateConnection())
+                using(var channel = connection.CreateModel())
+                {
+                    bool noAck = false;
+                    BasicGetResult res = channel.BasicGet("testQueue", noAck);
+
+                    if (res != null) 
+                    {
+                        IBasicProperties props = res.BasicProperties;
+                        byte[] body = res.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        channel.BasicAck(res.DeliveryTag, false);
+
+                        return message;
+                    } else
+                    {
+                        return "No messages";
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
